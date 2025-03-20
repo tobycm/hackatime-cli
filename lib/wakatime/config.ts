dc03c410abcebb3644cbdb7c8111c51121b019a7
Toby Cm <toby@vsus.app>
2025-03-20 00:41:01 +0000

    :fire: hotfix for windows machine

--- lib/wakatime/config.ts
+++ lib/wakatime/config.ts
@@ -1,3 +1,5 @@
+import { homedir } from "os";
+
 import { readFileSync } from "fs";
 import { parse } from "ini";
 
@@ -6,10 +8,17 @@ export interface WakatimeConfig<Filled extends boolean = false> {
   apiUrl: Filled extends true ? string : string | undefined;
 }
 
-export default function loadWakatimeConfig(configPath: string = `${process.env.HOME}/.wakatime.cfg`): WakatimeConfig {
-  const config = parse(readFileSync(configPath, "utf-8"));
-  return {
-    apiKey: config.settings.api_key,
-    apiUrl: config.settings.api_url,
-  };
+export default function loadWakatimeConfig(configPath: string = `${homedir()}/.wakatime.cfg`): WakatimeConfig {
+  try {
+    const config = parse(readFileSync(configPath, "utf-8"));
+    return {
+      apiKey: config.settings.api_key,
+      apiUrl: config.settings.api_url,
+    };
+  } catch (error) {
+    return {
+      apiUrl: undefined,
+      apiKey: undefined,
+    };
+  }
 }
